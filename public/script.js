b969d348741588eb54525a334a2af2f840531929
braxtonr <braxtonrobinson9@gmail.com>
2019-11-20 13:59:31 -0500

    Ass 14

--- public/script.js
+++ public/script.js
@@ -1,5 +1,78 @@
+async function showWorkouts(){
+    let workoutsJson = await fetch('workouts');
+    let workouts = await workoutsJson.json();
+    let workDiv = document.getElementById("workout");
+
+    for(i in workouts){
+        workDiv.append(getWorkElem(workouts[i]));
+    }
+}
+
+function getWorkElem(workout){
+    let workoutDiv = document.createElement("div");
+    workoutDiv.classList.add("workout");
+    let workoutTitle = document.createElement("h3");
+    workoutTitle.innerHTML = workout.id + ": " + workout.name;
+
+    let workoutP = document.createElement("p");
+    workoutP.innerHTML = `${workout.type} Exercise, ${workout.part} Body, Targets ${workout.muscles}`;
+
+    //create edit and delete links
+    let editLink = document.createElement("button");
+    editLink.href = "#edit-workout";
+    editLink.innerHTML = "Edit";
+    editLink.setAttribute("data-id", workout.id);
+    editLink.onclick = showEditWorkout;
+    let deleteLink = document.createElement("button");
+    deleteLink.href = "#";
+    deleteLink.innerHTML = "Delete";
+    deleteLink.setAttribute("data-id", workout.id);
+    deleteLink.onclick = deleteWorkout;
+    workoutP.append(editLink);
+    workoutP.append(deleteLink);
+
+    workoutDiv.append(workoutTitle);
+    workoutDiv.append(workoutP);
+    
+    return workoutDiv;
+}
+
+async function showEditWorkout(){
+    let id = this.getAttribute("data-id");
+    document.getElementById("edit-workout-id").innerHTML = id;
+
+    let response = await fetch(`workouts/${id}`);
+    let workout = await response.json();
+    document.getElementById("workout-edit-name").value = workout.name;
+    document.getElementById("workout-edit-type").value = workout.type;
+    document.getElementById("workout-edit-part").value = workout.part;
+    document.getElementById("workout-edit-muscles").value = workout.muscles;
+
+    return false;
+}
+
+async function deleteWorkout(){
+    const id = this.getAttribute("data-id");
+    
+    let response = await fetch(`workouts/${id}`, {
+        method: 'DELETE',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(workout),
+    });
+
+    if(response.status != 200){
+        console.log("Error adding workout");
+        return;
+    }
+
+    showWorkouts();
+    return false;
+}
+
 async function addWorkout(){
-    //get the song inforamtion
+    //get the workout inforamtion
     const wkName = document.getElementById("workout-name").value;
     const wkType = document.getElementById("workout-type").value;
     const wkPart = document.getElementById("workout-part").value;
@@ -9,7 +82,7 @@ async function addWorkout(){
 
     let workout = {"name": wkName, "type":wkType, "part":wkPart, "muscle-group": wkMuscles};
 
-    let response = await fetch('/workouts/', {
+    let response = await fetch('workouts/', {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json;charset=utf-8',
@@ -17,19 +90,46 @@ async function addWorkout(){
         body: JSON.stringify(workout),
     });
 
+    if(response.status != 200){
+        console.log("Error adding workout");
+        return;
+    }
+
+
     let result = await response.json();
-    console.log("Resposne from server: " + result);
+    showWorkouts();
+}
+
+async function editWorkout(){
+    let id = document.getElementById("edit-workout-id").textContent;
+    let name = document.getElementById("workout-edit-name").value;
+    let type = document.getElementById("workout-edit-type").value;
+    let part = document.getElementById("workout-edit-part").value;
+    let muscles = document.getElementById("workout-edit-muscles").value;
+    let workout = {"name":name, "type": type, "part": part, "muscles": muscles};
+
+    let response = await fetch(`workouts/${id}`, {
+        method: 'PUT',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(workout),
+    });
+
+    if(response.status != 200){
+        console.log("Error edditing workout");
+    }
+
+    showWorkouts();
 }
 
 
 window.onload = function(){
-
+    this.showWorkouts();
+    
     let addWkt = document.getElementById("add-btn");
     addWkt.onclick = this.addWorkout;
 
     let editBtn = document.getElementById("edit-btn");
-    //editBtn.onclick = ;
-
-    let deleteBtn = document.getElementById("del-btn");
-    //deleteBtn.onclick = ;
+    editBtn.onclick = editWorkout;
 }
--- server.js
+++ server.js
@@ -35,8 +35,24 @@ app.get('/',(req, res)=>{
     res.sendfile(__dirname + "/index.html");
 });
 
+function validateWorkout(workout){
+    const schema = {
+        name:Joi.string().min(3).required(),
+        type: Joi.string().max(5).required(),
+        part: Joi.string().required(),
+        muscles: Joi.string().required()
+    }
+
+    return Joi.validate(workout, schema);
+}
+
 app.post('/workouts', (req,res)=>{
-    const workouts = {
+    const result = validateWorkout(req.body);
+
+    if(result.error){
+        res.status(400).send(resutlt.error.details[0].message);
+    }
+    const workout = {
         id: workouts.length + 1,
         name: req.body.name,
         type: req.body.type,
@@ -44,9 +60,53 @@ app.post('/workouts', (req,res)=>{
         muscles: req.body.muscles
     }
 
-    workouts.push(song);
-    res.send(workouts);
+    workouts.push(workout);
+    res.send(workout);
 });
+
+app.put('/workouts/:id', (req,res)=>{
+    const requestedId = parseInt(req.params.id);
+    const workout = workouts.find(w =>w.id === requestedId);
+
+    //no workout with matchin id in array
+    if(!workout) {
+        res.status(404).send(`The workout with id ${requestedId} was not found`);
+        return;
+    }
+
+    //validating workout with schema
+    const result = validateSong(req.body);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    //update
+    workout.name = req.body.name;
+    workout.type = req.body.type;
+    workout.part = req.body.part;
+    workout.muscles = req.body.muscles;
+    res.send(workout);
+
+});
+
+app.delete('/workouts/:id',(req,res)=>{
+    const requestedId = parseInt(req.params.id);
+    const workout = workouts.find(w =>w.id === requestedId);
+
+    //no workout with matchin id in array
+    if(!workout) {
+        res.status(404).send(`The workout with id ${requestedId} was not found`);
+        return;
+    }
+
+    //workout exists so I can go forward and delete it
+    let index = workouts.indexOf(workout);
+    workouts.splice(index,1);
+    res.send(workout);
+});
+
 
 //listen
 const port = process.env.PORT || 3000;
